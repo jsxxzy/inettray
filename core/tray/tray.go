3d52ab7ba44088b22ef8ae828cf1783026fe8172
d1y <chenhonzhou@gmail.com>
2020-11-02 19:18:02 +0800

    完善本月统计, 将主逻辑分离到 tray/tray.go
    泪目...

---
+++ core/tray/tray.go
@@ -0,0 +1,204 @@
+// Author: d1y<chenhonzhou@gmail.com>
+// 实验性项目
+
+package tray
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/getlantern/systray"
+	"github.com/jsxxzy/inettray/core"
+	"github.com/jsxxzy/inettray/core/config"
+	"github.com/jsxxzy/inettray/icon"
+)
+
+var noLoginText = "登录"
+
+var loginText = "已登录"
+
+var windows = false
+
+// 判断是否为 `windows` 系统
+func isWindows() bool {
+	return runtime.GOOS == "windows"
+}
+
+// New tray
+func New(tray Tray) *Tray {
+	return &tray
+}
+
+// Tray 托盘
+type Tray struct {
+	Login          *systray.MenuItem // 一层菜单的登录
+	ReloadInfo     *systray.MenuItem // 登录=> 刷新信息
+	WindowLogin    *systray.MenuItem // 登录=> 登录(windows)
+	Ipv4           *systray.MenuItem // 登录=> ipv4
+	Flow           *systray.MenuItem // 登录=> 流量
+	Duration       *systray.MenuItem // 登录=> 时长
+	MonthFlow      *systray.MenuItem // 登录=> 本月已用
+	Logout         *systray.MenuItem // 注销
+	ConfButton     *systray.MenuItem // 一层菜单的配置
+	CurrUser       *systray.MenuItem // 配置=> 用户
+	ReloadUser     *systray.MenuItem // 配置=> 刷新本地用户
+	OpenConfigCopy *systray.MenuItem // 配置=> 打开配置文件(windows)
+	HelpButton     *systray.MenuItem // 一层的帮助
+	MQuit          *systray.MenuItem // 退出
+
+	FlowCopy float64 // 流量值
+	Ipv4Copy string  // ipv4
+}
+
+// SetMenuIcon 设置主菜单图标
+func (tray *Tray) SetMenuIcon(iconData []byte) {
+	systray.SetIcon(iconData)
+}
+
+// SetMenuToolTip 设置菜单
+func (tray *Tray) SetMenuToolTip(toolTip string) {
+	systray.SetTooltip(toolTip)
+}
+
+// SetMenuTitle 设置菜单标题
+func (tray *Tray) SetMenuTitle(title string) {
+	systray.SetTitle(title)
+}
+
+// SetMenuLoginFlag 设置菜单登录
+func (tray *Tray) SetMenuLoginFlag(flag bool) {
+	var s = noLoginText
+	if flag {
+		s = loginText
+	}
+	systray.SetTitle(s)
+}
+
+// SetInfo 设置信息
+func (tray *Tray) SetInfo() error {
+	tmpData, tmpErr := core.GetInfo()
+	if tmpErr == nil && tmpData.Time != "0" {
+		tray.SetFlowMB(tmpData.FlowMB)
+		tray.SetIpv4(tmpData.Ipv4)
+		tray.Ipv4.SetTitle(tmpData.Ipv4)
+		tray.Flow.SetTitle(tmpData.Flow)
+		tray.Duration.SetTitle(tmpData.Time)
+		tray.Login.SetTitle(loginText)
+		tray.SetMonthTotalFlowTitle()
+		tray.SetWindowsLoginButtonFlag(false)
+		return nil
+	}
+	return tray.SetInfoZero()
+}
+
+// SetInfoZero 设置初始化信息
+func (tray *Tray) SetInfoZero() error {
+	tray.Login.SetTitle(noLoginText)
+	tray.Ipv4.SetTitle("内网ip")
+	tray.Flow.SetTitle("流量")
+	tray.Duration.SetTitle("使用时长")
+	tray.SetFlowMB(0)
+	tray.SetIpv4("")
+	tray.SetMonthTotalFlowTitle()
+	return nil
+}
+
+// SetWindowsLoginButtonFlag 设置 `windows` 登录按钮显示和隐藏
+func (tray *Tray) SetWindowsLoginButtonFlag(flag bool) {
+	if flag {
+		tray.WindowLogin.Show()
+	} else {
+		tray.WindowLogin.Hide()
+	}
+}
+
+// SetWindowsOpenConfig 设置 `windows` 打开配置文件显示和隐藏
+func (tray *Tray) SetWindowsOpenConfig(flag bool) {
+	if flag {
+		tray.OpenConfigCopy.Show()
+	} else {
+		tray.OpenConfigCopy.Hide()
+	}
+}
+
+// SetMonthTotalFlowTitle 设置本月已用
+func (tray *Tray) SetMonthTotalFlowTitle() {
+	var f = core.GetHumanMonthTotalFlow()
+	var s = fmt.Sprintf("本月已用: %v", f)
+	tray.MonthFlow.SetTitle(s)
+}
+
+// LoginWith 登录
+func (tray *Tray) LoginWith() error {
+	if err := core.Login(); err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	return tray.SetInfo()
+}
+
+// CopyIPV4 set `ipv4` to clipboard
+func (tray *Tray) CopyIPV4() {
+	var ipv4 = tray.Ipv4Copy
+	if len(ipv4) >= 1 {
+		core.SetClipboard(ipv4)
+	}
+}
+
+// SetFlowMB 设置流量值
+func (tray *Tray) SetFlowMB(f float64) {
+	tray.FlowCopy = f
+}
+
+// SetIpv4 设置`ipv4`
+func (tray *Tray) SetIpv4(ipv4 string) {
+	tray.Ipv4Copy = ipv4
+}
+
+// SetUsername 设置`username`
+func (tray *Tray) SetUsername() {
+	var username = core.EasyGetLocalAuthUsername()
+	tray.CurrUser.SetTitle(username)
+}
+
+// LogoutWith 注销
+func (tray *Tray) LogoutWith() {
+	var err = core.Logout()
+	if err == nil {
+		var f = tray.FlowCopy
+		core.SetLocalMonthTotalFlow(f)
+		tray.SetMonthTotalFlowTitle()
+		tray.SetInfoZero()
+		if windows {
+			tray.SetWindowsLoginButtonFlag(true)
+		}
+	}
+}
+
+// InitDisableMenu 初始化禁用某些`menu`
+func (tray *Tray) InitDisableMenu() {
+	tray.MonthFlow.Disable()
+	tray.Flow.Disable()
+	tray.Duration.Disable()
+	tray.CurrUser.Disable()
+}
+
+// Init 初始化
+func (tray *Tray) Init() {
+	tray.InitDisableMenu()
+
+	tray.SetMenuIcon(icon.Data)
+	tray.SetMenuToolTip(config.MainTitleTip)
+
+	tray.SetInfo()
+	tray.SetUsername()
+
+	if !windows {
+		tray.SetWindowsOpenConfig(false)
+		tray.SetWindowsLoginButtonFlag(false)
+	}
+}
+
+func init() {
+	windows = isWindows()
+}
--- main.go
+++ main.go
@@ -5,93 +5,52 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
 	"github.com/getlantern/systray"
 	"github.com/jsxxzy/inettray/core"
 	"github.com/jsxxzy/inettray/core/config"
-	"github.com/jsxxzy/inettray/icon"
+	"github.com/jsxxzy/inettray/core/tray"
 )
-
-var noLoginText = "登录"
-
-var loginText = "已登录"
 
 func main() {
 	systray.Run(onReady, onExit)
 }
 
-var windows = false
-
-// 判断是否为 `windows` 系统
-func isWindows() bool {
-	return runtime.GOOS == "windows"
-}
-
 func onReady() {
 
-	tmpData, tmpErr := core.GetInfo()
-	localAuthUsername := core.EasyGetLocalAuthUsername()
-
-	systray.SetIcon(icon.Data)
-
-	systray.SetTooltip(config.MainTitleTip)
-	// systray.SetTitle(config.AppName)
-	loginButton := systray.AddMenuItem(noLoginText, config.LoginTip)
-
+	loginButton := systray.AddMenuItem("登录", config.LoginTip)
 	reloadInfoButton := loginButton.AddSubMenuItem("刷新信息", "获取使用信息")
-
-	wnidowsLoginButton := loginButton.AddSubMenuItem("登录", "")
-
-	if !windows || tmpErr == nil {
-		wnidowsLoginButton.Hide()
-	}
-
-	// ip
+	windowsLoginButton := loginButton.AddSubMenuItem("登录", "")
 	ipv4 := loginButton.AddSubMenuItem("内网ip", "单击可复制ipv4地址")
-	// 流量
 	flow := loginButton.AddSubMenuItem("流量", "使用流量")
-	// 使用时长
 	duration := loginButton.AddSubMenuItem("使用时长", "使用时长")
-
 	monthFlow := loginButton.AddSubMenuItem("本月已用", "本月已用")
-
-	monthFlow.Disable()
-
-	var flowCopy float64 = 0
-
-	if tmpErr == nil {
-		ipv4.SetTitle(tmpData.Ipv4)
-		flowCopy = tmpData.FlowMB
-		var f = core.GetHumanMonthTotalFlow()
-		var s = fmt.Sprintf("本月已用: %v", f)
-		monthFlow.SetTitle(s)
-		flow.SetTitle(tmpData.Flow)
-		duration.SetTitle(tmpData.Time)
-		loginButton.SetTitle(loginText)
-	}
-
-	flow.Disable()
-	duration.Disable()
-
 	logoutButton := systray.AddMenuItem("注销", config.LogoutTip)
-
 	confButton := systray.AddMenuItem("配置", config.ConfigTip)
-	currUser := confButton.AddSubMenuItem(localAuthUsername, "当前的账号")
-	currUser.Disable()
-
+	currUser := confButton.AddSubMenuItem("", "当前的账号")
 	reloadUser := confButton.AddSubMenuItem("刷新本地用户", "刷新本地用户")
-
 	openConfigCopy := confButton.AddSubMenuItem("打开配置文件", "打开配置文件")
-
-	if !windows {
-		openConfigCopy.Hide()
-	}
-
 	helpButton := systray.AddMenuItem("帮助", config.HelpTip)
 	mQuit := systray.AddMenuItem("退出", config.ExitTip)
 
-	var ipv4copy = ""
+	var App = tray.New(tray.Tray{
+		Login:          loginButton,
+		ReloadInfo:     reloadInfoButton,
+		WindowLogin:    windowsLoginButton,
+		Ipv4:           ipv4,
+		Flow:           flow,
+		Duration:       duration,
+		MonthFlow:      monthFlow,
+		Logout:         logoutButton,
+		ConfButton:     confButton,
+		CurrUser:       currUser,
+		ReloadUser:     reloadUser,
+		OpenConfigCopy: openConfigCopy,
+		HelpButton:     helpButton,
+		MQuit:          mQuit,
+	})
+
+	App.Init()
 
 	go func() {
 		<-mQuit.ClickedCh
@@ -104,100 +63,31 @@ func onReady() {
 		for {
 			select {
 			case <-monthFlow.ClickedCh: // 本月已用
-				var f = core.GetHumanMonthTotalFlow()
-				var s = fmt.Sprintf("本月已用: %v", f)
-				monthFlow.SetTitle(s)
+				App.SetMonthTotalFlowTitle()
 			case <-ipv4.ClickedCh: // ipv4
-				if len(ipv4copy) >= 1 {
-					core.SetClipboard(ipv4copy)
-				}
+				App.CopyIPV4()
 			case <-reloadInfoButton.ClickedCh: // 获取使用信息
-				data, err := core.GetInfo()
-				if err != nil {
-					loginButton.SetTitle(noLoginText)
-					ipv4.SetTitle("内网ip")
-					flow.SetTitle("流量")
-					duration.SetTitle("使用时长")
-					flowCopy = 0
-					ipv4copy = ""
-				} else {
-					loginButton.SetTitle(loginText)
-					ipv4.SetTitle(data.Ipv4)
-					ipv4copy = data.Ipv4
-					flowCopy = data.FlowMB
-					flow.SetTitle(data.Flow)
-					duration.SetTitle(data.Time)
-					var f = core.GetHumanMonthTotalFlow()
-					var s = fmt.Sprintf("本月已用: %v", f)
-					monthFlow.SetTitle(s)
-				}
+				App.SetInfo()
 			case <-reloadUser.ClickedCh: // 获取本地用户
-				var username = core.EasyGetLocalAuthUsername()
-				currUser.SetTitle(username)
+				App.SetUsername()
 			case <-helpButton.ClickedCh: // 帮助
 				core.OpenHelp()
 			case <-openConfigCopy.ClickedCh:
 				core.OpenConfig()
 			case <-confButton.ClickedCh: // 配置
 				core.OpenConfig()
-
-			case <-wnidowsLoginButton.ClickedCh: // 登录
-				// TODO: 重复代码
-				if err := core.Login(); err != nil {
-					fmt.Println(err.Error())
-				} else {
-					tmpData, tmpErr := core.GetInfo()
-					if tmpErr == nil && tmpData.Time != "0" {
-						flowCopy = tmpData.FlowMB
-						ipv4copy = tmpData.Ipv4
-						ipv4.SetTitle(tmpData.Ipv4)
-						flow.SetTitle(tmpData.Flow)
-						duration.SetTitle(tmpData.Time)
-						loginButton.SetTitle(loginText)
-					}
-					wnidowsLoginButton.Hide()
-				}
+			case <-windowsLoginButton.ClickedCh: // 登录
+				App.LoginWith()
 			case <-loginButton.ClickedCh: // 登录
-				if err := core.Login(); err != nil {
-					fmt.Println(err.Error())
-				} else {
-					tmpData, tmpErr := core.GetInfo()
-					if tmpErr == nil && tmpData.Time != "0" {
-						flowCopy = tmpData.FlowMB
-						ipv4copy = tmpData.Ipv4
-						ipv4.SetTitle(tmpData.Ipv4)
-						flow.SetTitle(tmpData.Flow)
-						duration.SetTitle(tmpData.Time)
-						loginButton.SetTitle(loginText)
-					}
-				}
+				App.LoginWith()
 			case <-logoutButton.ClickedCh: // 注销
-				if core.Logout() == nil {
-					ipv4.SetTitle("内网ip")
-					ipv4copy = ""
-					core.SetLocalMonthTotalFlow(flowCopy)
-					flowCopy = 0
-					var fm = fmt.Sprintf("本月已用: %v", core.GetHumanMonthTotalFlow())
-					monthFlow.SetTitle(fm)
-					flow.SetTitle("流量")
-					duration.SetTitle("使用时长")
-					loginButton.SetTitle(noLoginText)
-					if windows {
-						wnidowsLoginButton.Show()
-					}
-				}
+				App.LogoutWith()
 			}
 		}
 	}()
 
-	// Sets the icon of a menu item. Only available on Mac and Windows.
-	// mQuit.SetIcon(icon.Data)
 }
 
 func onExit() {
-	// clean up here
-}
-
-func init() {
-	windows = isWindows()
+	fmt.Println("已退出")
 }
